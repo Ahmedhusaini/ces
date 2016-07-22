e3f5d00f141cdb417de618e6e0b57ebd72f2f986
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-07-21 22:56:45 -0700

    single file upload method, completed but not tested

--- FinalTemplate/source/Functions/JFunctions.cs
+++ FinalTemplate/source/Functions/JFunctions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Web.UI.WebControls;
-using System.Net.Mail;
-using System.Net;
 using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI.WebControls;
+
 namespace FinalTemplate.source.Functions
 {
     public class JFunctions
@@ -16,18 +18,18 @@ namespace FinalTemplate.source.Functions
         {
             return Convert.ToString(System.DateTime.Today.ToString("t"));
         }
-        public static void SendEmail(string To,string Subject,string MessageBody)
+        public static void SendEmail(string To, string Subject, string MessageBody)
         {
             string smtpUsername = "jahangeer.ahmed11@gmail.com";
             //i changed my original password to this to check the email, later i will change this back to my original password.
             //if you want to send email from your own email id change the id and password to yours and email will be sent through
             //your email address.
             string smtpPassword = "abbasikhan";
-            int smtpPort = 25; 
+            int smtpPort = 25;
             try
             {
                 MailMessage message = new MailMessage();
-                message.From = new MailAddress(smtpUsername,"CES - User Registration Team");
+                message.From = new MailAddress(smtpUsername, "CES - User Registration Team");
                 message.To.Add(To);
                 message.Subject = Subject;
                 message.Body = MessageBody;
@@ -35,16 +37,47 @@ namespace FinalTemplate.source.Functions
                 SmtpClient smtp = new SmtpClient();
                 smtp.Host = "smtp.gmail.com";
                 smtp.Port = smtpPort;
-                NetworkCredential network = new NetworkCredential(smtpUsername,smtpPassword);
+                NetworkCredential network = new NetworkCredential(smtpUsername, smtpPassword);
                 smtp.UseDefaultCredentials = false;
                 smtp.Credentials = network;
-                 
+
                 smtp.Send(message);
             }
             catch (Exception ex)
-            { 
-                
+            {
+
             }
+        }
+        //File size limit in bytes
+        public string UploadSingleFile(FileUpload objFileUpload, string filePath, int fileSizeLimit)
+        {
+            string fullPath = "~/" + filePath;
+            string savedPath = string.Empty, returnvalue = string.Empty;
+            returnvalue = "null";
+            if (objFileUpload.HasFile)
+            {
+                int currentFileSize = objFileUpload.PostedFile.ContentLength;
+                if (currentFileSize > fileSizeLimit)
+                {
+                    returnvalue = "value is over the limit.";
+                }
+                else
+                {
+                    savedPath = Path.Combine(fullPath, objFileUpload.FileName);
+                    try
+                    {
+                        objFileUpload.PostedFile.SaveAs(HttpContext.Current.Server.MapPath(savedPath));
+                        returnvalue = savedPath;
+                    }
+                    catch (Exception exception)
+                    {
+                        returnvalue = exception.ToString();
+                    }
+                }
+
+            }
+            return returnvalue;
+
         }
         public static void BindDropDownList(DropDownList ddl, string displayField, string valueField, string query)
         {
@@ -62,7 +95,7 @@ namespace FinalTemplate.source.Functions
             finally
             {
                 myDatabase.CloseConnection();
-                myDatabase.obj_sqlcommand.Dispose();                
+                myDatabase.obj_sqlcommand.Dispose();
             }
         }
     }
