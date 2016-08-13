7639294f8cc771814efd46e88fa414c9a8e47849
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-08-13 01:57:56 -0700

    as

--- FinalTemplate/source/Database/Database.cs
+++ FinalTemplate/source/Database/Database.cs
@@ -182,7 +182,13 @@ namespace FinalTemplate.source.Database
                     {
                         //if black listed keywords are filtered successfully. Then converting actual value to respective column type match as sql server database table
                         if (Jvalidate.FilterBlackLIstKeywords(columnArray))
-                            this.obj_sqlcommand.Parameters.AddWithValue("@" + ColumnNames[i].ToString(), Convert.ToInt32(Jvalidate.RemoveHtmlTags(ColumnValues[i].ToString())));
+                        {
+                            this.obj_sqlcommand.Parameters.AddWithValue("@" + ColumnNames[i].ToString(),
+                                Convert.ToInt32(Jvalidate.RemoveHtmlTags(ColumnValues[i].ToString())));
+
+
+
+                        }
                         else
                             break;
                     }
