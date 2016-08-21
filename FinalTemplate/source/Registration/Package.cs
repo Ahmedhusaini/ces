de230d8080bb4ba088351216d8133efacf0c7860
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-08-21 16:56:44 +0500

    package details

--- FinalTemplate/source/Registration/Package.cs
+++ FinalTemplate/source/Registration/Package.cs
@@ -14,12 +14,12 @@ namespace FinalTemplate.source.Registration
         public int PackageTypeID { get; set; }
         public bool OnlineAdmission { get; set; }
         public bool TeacherProfile { get; set; }
-        public string PostJobVacancies { get; set; }
+        public bool PostJobVacancies { get; set; }
         public string Emails { get; set; }
         public string AttendanceForStaff { get; set; }
         public string AttendanceForStudent { get; set; }
         public bool Results { get; set; }
-        public bool FreeStructure { get; set; }
+        public bool FeesStructure { get; set; }
         public string Syllabus { get; set; }
         public string DateSheets { get; set; }
         public string Events { get; set; }
@@ -27,14 +27,16 @@ namespace FinalTemplate.source.Registration
         public string Reports { get; set; }
         public bool StudentOfTheMonth { get; set; }
         public bool AssignmentAndHomework { get; set; }
-        public string SMS { get; set; }
-        public string EmailAccounts { get; set; }
+        public int SMS { get; set; }
+        public int EmailAccounts { get; set; }
         public string Notifications { get; set; }
-        public string ComparisonOfSchools { get; set; }
+        public int ComparisonOfSchools { get; set; }
         public bool SchoolRating { get; set; }
         public bool SchoolPublicProfile { get; set; }
         public bool SchoolIntroductionVideo { get; set; }
+        public string ActiveUsers { get; set; }
         public string SameAccountForOtherBranch { get; set; }
+        public string SchoolToSchoolMessage { get; set; }
         public string DatabaseBackup { get; set; }
         public string SecurityAndConfidentiality { get; set; }
         public string DataAvailability { get; set; }
@@ -48,11 +50,124 @@ namespace FinalTemplate.source.Registration
 
         #endregion
         //method for assigning these attributes values and insert data into database according to selected package.
-        public void InitializePackageAttributes(string packageName)
+        public void InitializePackageAttributes(string packageName, string schoolID)
         {
             if (packageName == "Silver")
             {
-                PackageID = Convert.ToInt32(myDatabase.GetLastValueByColumnName("package_id", "tbl_packages"));
+                PackageID = Convert.ToInt32(myDatabase.GetLastValueByColumnName("package_id", "tbl_packages")) + 1;
+                PackageTypeID = 1;
+                OnlineAdmission = true;
+                TeacherProfile = true;
+                PostJobVacancies = false;
+                Emails = "700";
+                AttendanceForStaff = "Manual";
+                AttendanceForStudent = "No";
+                Results = true;
+                FeesStructure = true;
+                Syllabus = "Yes - allow 1 change";
+                DateSheets = "unlimited";
+                Events = "3";
+                Gallery = "no";
+                Reports = "Admin can view";
+                StudentOfTheMonth = true;
+                AssignmentAndHomework = true;
+                SMS = 1500;
+                EmailAccounts = 5;
+                Notifications = "Email";
+                ComparisonOfSchools = 5;
+                SchoolRating = true;
+                SchoolPublicProfile = false;
+                SchoolIntroductionVideo = false;
+                ActiveUsers = "No";
+                SchoolToSchoolMessage = "No";
+                SameAccountForOtherBranch = "No";
+                DatabaseBackup = "One when package expires";
+                SecurityAndConfidentiality = "High";
+                DataAvailability = "High";
+                PackageCustomization = false;
+                PackageTerminationReport = true;
+                OnlineSupport = "Yes, by Email";
+                DataFilters = "No";
+                AdminPanelType = "Basic";
+                SchoolID = schoolID;
+            }
+            else if (packageName == "Gold")
+            {
+                PackageID = Convert.ToInt32(myDatabase.GetLastValueByColumnName("package_id", "tbl_packages")) + 1;
+                PackageTypeID = 1;
+                OnlineAdmission = true;
+                TeacherProfile = true;
+                PostJobVacancies = false;
+                Emails = "700";
+                AttendanceForStaff = "Manual";
+                AttendanceForStudent = "No";
+                Results = true;
+                FeesStructure = true;
+                Syllabus = "Yes - allow 1 change";
+                DateSheets = "unlimited";
+                Events = "3";
+                Gallery = "no";
+                Reports = "Admin can view";
+                StudentOfTheMonth = true;
+                AssignmentAndHomework = true;
+                SMS = 1500;
+                EmailAccounts = 5;
+                Notifications = "Email";
+                ComparisonOfSchools = 5;
+                SchoolRating = true;
+                SchoolPublicProfile = false;
+                SchoolIntroductionVideo = false;
+                ActiveUsers = "No";
+                SchoolToSchoolMessage = "No";
+                SameAccountForOtherBranch = "No";
+                DatabaseBackup = "One when package expires";
+                SecurityAndConfidentiality = "High";
+                DataAvailability = "High";
+                PackageCustomization = false;
+                PackageTerminationReport = true;
+                OnlineSupport = "Yes, by Email";
+                DataFilters = "No";
+                AdminPanelType = "Basic";
+                SchoolID = schoolID;
+            }
+            else if (packageName == "Platinum")
+            {
+                PackageID = Convert.ToInt32(myDatabase.GetLastValueByColumnName("package_id", "tbl_packages")) + 1;
+                PackageTypeID = 1;
+                OnlineAdmission = true;
+                TeacherProfile = true;
+                PostJobVacancies = false;
+                Emails = "700";
+                AttendanceForStaff = "Manual";
+                AttendanceForStudent = "No";
+                Results = true;
+                FeesStructure = true;
+                Syllabus = "Yes - allow 1 change";
+                DateSheets = "unlimited";
+                Events = "3";
+                Gallery = "no";
+                Reports = "Admin can view";
+                StudentOfTheMonth = true;
+                AssignmentAndHomework = true;
+                SMS = 1500;
+                EmailAccounts = 5;
+                Notifications = "Email";
+                ComparisonOfSchools = 5;
+                SchoolRating = true;
+                SchoolPublicProfile = false;
+                SchoolIntroductionVideo = false;
+                ActiveUsers = "No";
+                SchoolToSchoolMessage = "No";
+                SameAccountForOtherBranch = "No";
+                DatabaseBackup = "One when package expires";
+                SecurityAndConfidentiality = "High";
+                DataAvailability = "High";
+                PackageCustomization = false;
+                PackageTerminationReport = true;
+                OnlineSupport = "Yes, by Email";
+                DataFilters = "No";
+                AdminPanelType = "Basic";
+                SchoolID = schoolID;
             }
         }
     }
