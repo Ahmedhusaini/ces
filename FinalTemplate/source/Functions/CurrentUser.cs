e2a3f56dba085af2ef88b952e2638d4b9abdf331
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-08-30 01:37:51 +0500

    current user class

--- FinalTemplate/source/Functions/CurrentUser.cs
+++ FinalTemplate/source/Functions/CurrentUser.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using FinalTemplate.source.Database;
-namespace FinalTemplate.source.Functions
+﻿namespace FinalTemplate.source.Functions
 {
     public class CurrentUser
     {
-       
+        private Database.Database myDatabase = new Database.Database("cesConnectionString");
+
+        public string AuthorizedID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PrimaryEmailAddress { get; set; }
+        public string SecondaryEmailAddress { get; set; }
+        public int UserTypeID { get; set; }
+        public string UserType { get; set; }
+        public int LoginCount { get; set; }
+        public string GeneralID { get; set; }
+        public string LastLoginDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Nationality { get; set; }
+        public string Gender { get; set; }
+        public string Photo { get; set; }
+        public string Religion { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
     }
 }
