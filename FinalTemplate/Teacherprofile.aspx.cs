02fb50bba24774799dac95c945f34a522ea41787
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-08-29 14:06:13 -0700

    profile

--- FinalTemplate/Teacherprofile.aspx.cs
+++ FinalTemplate/Teacherprofile.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.IO;
 using System.Web.UI.WebControls;
 
 namespace FinalTemplate
@@ -13,5 +14,37 @@ namespace FinalTemplate
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(FileUpload1.HasFile)
+            {
+                if((FileUpload1.PostedFile.ContentType=="image/jpeg")||
+                (FileUpload1.PostedFile.ContentType=="image/png")||
+                (FileUpload1.PostedFile.ContentType=="image/bmp"))
+                {
+                    if(Convert.ToInt64(FileUpload1.PostedFile.ContentLength) < 1000000)
+                    {
+                        string folder = Path.Combine("~/images/", User.Identity.Name);
+                        if (!Directory.Exists(folder))
+                            Directory.CreateDirectory(folder);
+
+                        string extension = Path.GetExtension(FileUpload1.FileName);
+                        string uniquefilename = Path.ChangeExtension(FileUpload1.FileName, DateTime.Now.Ticks.ToString());
+
+                        FileUpload1.SaveAs(Path.Combine(folder, uniquefilename + extension));
+                        Label1.Text="SUCCESSFULLY UPLOADED" + FileUpload1.FileName;
+                    }
+                    else
+                        Label1.Text = "FILE MUST BE LESS THAN 10 MB";
+                }
+                    else
+                        Label1.Text="FILE MUST BE JPEG,JPG,BMP";
+                    }
+                   
+                   else
+                        Label1.Text="NO FILE SELECTED";
+               
+            }
+        }
     }
-}
