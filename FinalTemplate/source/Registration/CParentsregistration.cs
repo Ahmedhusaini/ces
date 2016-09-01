c1b9f7bfa8d4614f7df2a5be9494f0c9dbf52def
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-09-01 14:52:34 +0500

    sheeri's working

--- FinalTemplate/source/Registration/CParentsregistration.cs
+++ FinalTemplate/source/Registration/CParentsregistration.cs
@@ -1,4 +1,4 @@
-﻿    using FinalTemplate.source.Functions;
+﻿using FinalTemplate.source.Functions;
 using System;
 using System.Data;
 using System.Data.SqlClient;
@@ -8,17 +8,17 @@ namespace FinalTemplate.source.Registration
 {
     public class CParentsregistraion
     {
-        private Database.Database mdata = new Database.Database("cesConnectionString3");
+        private Database.Database mdata = new Database.Database("cesConnectionString");
 
         public string Registerparents(string firstname, string lastname, string nationality, string gender, string photo,
-            string religion,string phone, string address, string officeaddress, string officeno, string cnicno,
+            string religion, string phone, string address, string officeaddress, string officeno, string cnicno,
             string postdesignation, string salaryanum, int country_id, int city_id, int postalcode, string username,
             string password, int accountpin, string primaryemail, string secondaryemail)
         {
             string locationid = mdata.GetLastValueByColumnName("loc_id", "tbl_location");
             string genaralid = mdata.GetLastValueByColumnName("General_Id", "tbl_general");
             string dobid = mdata.GetLastValueByColumnName("dob_id", "tbl_dob");
-        
+
 
 
 
@@ -27,9 +27,9 @@ namespace FinalTemplate.source.Registration
             SqlParameter parentidParameter = new SqlParameter("@Parent_id ", SqlDbType.VarChar, 50);
             SqlParameter generalidParameter = new SqlParameter("@general_id  ", SqlDbType.Int);
             SqlParameter firstnameParameter = new SqlParameter("@firstname ", SqlDbType.VarChar, 50);
-            SqlParameter lastnameParameter = new SqlParameter("@lastname ", SqlDbType.VarChar,50);
+            SqlParameter lastnameParameter = new SqlParameter("@lastname ", SqlDbType.VarChar, 50);
             SqlParameter nationalityParameter = new SqlParameter("@nationality ", SqlDbType.VarChar, 50);
-            SqlParameter genderParameter = new SqlParameter("@gender ", SqlDbType.VarChar,7);
+            SqlParameter genderParameter = new SqlParameter("@gender ", SqlDbType.VarChar, 7);
             SqlParameter photoParameter = new SqlParameter("@photo  ", SqlDbType.VarChar, 50);
             SqlParameter religionParameter = new SqlParameter("@religion  ", SqlDbType.VarChar, 20);
             SqlParameter phoneParameter = new SqlParameter("@phone   ", SqlDbType.VarChar, 50);
@@ -40,7 +40,7 @@ namespace FinalTemplate.source.Registration
             SqlParameter postdesignationParameter = new SqlParameter("@Post_Designation ", SqlDbType.VarChar, 50);
             SqlParameter salaryanumParameter = new SqlParameter("@Salary_Anum    ", SqlDbType.VarChar, 20);
             SqlParameter generalidoutParameter = new SqlParameter("@general_id_out ", SqlDbType.Int);
-            SqlParameter parentidoutParameter = new SqlParameter("@parent_id_out ", SqlDbType.VarChar,50);                  
+            SqlParameter parentidoutParameter = new SqlParameter("@parent_id_out ", SqlDbType.VarChar, 50);
             SqlParameter dobidParameter = new SqlParameter("@dob_id ", SqlDbType.Int);
             SqlParameter dayParameter = new SqlParameter("@day", SqlDbType.Int);
             SqlParameter monthParameter = new SqlParameter("@month ", SqlDbType.Int);
@@ -49,10 +49,10 @@ namespace FinalTemplate.source.Registration
             SqlParameter locidParameter = new SqlParameter("@loc_id", SqlDbType.Int);
             SqlParameter countryidParameter = new SqlParameter("@country_id", SqlDbType.Int);
             SqlParameter cityidParameter = new SqlParameter("@city_id", SqlDbType.Int);
-            SqlParameter postalParameter = new SqlParameter("@postal_code", SqlDbType.Int);                 
+            SqlParameter postalParameter = new SqlParameter("@postal_code", SqlDbType.Int);
             SqlParameter locidoutidParameter = new SqlParameter("@loc_id_out ", SqlDbType.Int);
-            SqlParameter authorizedidParameter = new SqlParameter("@authorized_id ", SqlDbType.VarChar,20);
-            SqlParameter usernameParameter = new SqlParameter("@username", SqlDbType.VarChar,50);
+            SqlParameter authorizedidParameter = new SqlParameter("@authorized_id ", SqlDbType.VarChar, 20);
+            SqlParameter usernameParameter = new SqlParameter("@username", SqlDbType.VarChar, 50);
             SqlParameter passwordParameter = new SqlParameter("@password", SqlDbType.VarChar, 50);
             SqlParameter accountpinParameter = new SqlParameter("@account_pin", SqlDbType.Int);
             SqlParameter primaryemailParameter = new SqlParameter("@primary_email", SqlDbType.VarChar, 50);
@@ -60,13 +60,13 @@ namespace FinalTemplate.source.Registration
             SqlParameter usertypeParameter = new SqlParameter("@usertype_id", SqlDbType.Int);
             SqlParameter logincountParameter = new SqlParameter("@login_count", SqlDbType.Int);
             SqlParameter lastloginParameter = new SqlParameter("@last_login_date", SqlDbType.Date);
-            SqlParameter authorizedidoutParameter = new SqlParameter("@authorized_id_out", SqlDbType.VarChar,20);
+            SqlParameter authorizedidoutParameter = new SqlParameter("@authorized_id_out", SqlDbType.VarChar, 20);
 
 
 
 
             parentidParameter.Value = GenerateParentID(firstname, lastname);
-            generalidParameter.Value = Convert.ToInt32(genaralid)+1 ;
+            generalidParameter.Value = Convert.ToInt32(genaralid) + 1;
             firstnameParameter.Value = firstname;
             lastnameParameter.Value = lastname;
             nationalityParameter.Value = nationality;
@@ -80,18 +80,18 @@ namespace FinalTemplate.source.Registration
             cnicnoParameter.Value = cnicno;
             postdesignationParameter.Value = postdesignation;
             salaryanumParameter.Value = salaryanum;
-            generalidoutParameter.Direction= ParameterDirection.Output;
-            parentidoutParameter.Direction=ParameterDirection.Output;
-            dobidParameter.Value = Convert.ToInt32(dobid)+1;
+            generalidoutParameter.Direction = ParameterDirection.Output;
+            parentidoutParameter.Direction = ParameterDirection.Output;
+            dobidParameter.Value = Convert.ToInt32(dobid) + 1;
             dayParameter.Value = Convert.ToInt32(Jfunctionparents.GetSystemDate().Substring(2, 2));
             monthParameter.Value = Convert.ToInt32(Jfunctionparents.GetSystemDate().Substring(0, 1));
             yearParameter.Value = Convert.ToInt32(Jfunctionparents.GetSystemDate().Substring(5, 4));
-            dobidoutParameter.Direction=ParameterDirection.Output;
+            dobidoutParameter.Direction = ParameterDirection.Output;
             locidParameter.Value = Convert.ToInt32(locationid) + 1;
             countryidParameter.Value = country_id;
             cityidParameter.Value = city_id;
             postalParameter.Value = postalcode;
-            locidoutidParameter.Direction=ParameterDirection.Output;
+            locidoutidParameter.Direction = ParameterDirection.Output;
             authorizedidParameter.Value = GenerateAuthorizedID(username, accountpin.ToString());
             usernameParameter.Value = username;
             passwordParameter.Value = password;
@@ -101,7 +101,7 @@ namespace FinalTemplate.source.Registration
             usertypeParameter.Value = 3;
             logincountParameter.Value = 0;
             lastloginParameter.Value = Convert.ToDateTime(Jfunctionparents.GetSystemDate());
-            authorizedidoutParameter.Direction=ParameterDirection.Output;
+            authorizedidoutParameter.Direction = ParameterDirection.Output;
 
 
             mdata.obj_sqlcommand.Parameters.Add(parentidParameter);
@@ -141,7 +141,7 @@ namespace FinalTemplate.source.Registration
             mdata.obj_sqlcommand.Parameters.Add(logincountParameter);
             mdata.obj_sqlcommand.Parameters.Add(lastloginParameter);
             mdata.obj_sqlcommand.Parameters.Add(authorizedidoutParameter);
-     
+
 
             try
             {
@@ -175,7 +175,7 @@ namespace FinalTemplate.source.Registration
             return Convert.ToString(Username.Substring(0, 3) + AccountPin.Substring(0, 3));
         }
 
-      
+
 
         private string GenerateParentID(string Firstname, string Lastname)
         {
@@ -196,6 +196,6 @@ namespace FinalTemplate.source.Registration
         }
 
 
-       
+
     }
 }
