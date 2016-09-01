f44927d84d523682d764108babde8fc6f53fe671
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-09-01 13:52:11 -0700

    school

--- FinalTemplate/TeacherRegistration.aspx.designer.cs
+++ FinalTemplate/TeacherRegistration.aspx.designer.cs
@@ -148,6 +148,24 @@ namespace FinalTemplate {
         protected global::System.Web.UI.WebControls.FileUpload fileupload;
         
         /// <summary>
+        /// DropDownList3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
+        
+        /// <summary>
+        /// SqlDataSource3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource3;
+        
+        /// <summary>
         /// user control.
         /// </summary>
         /// <remarks>
