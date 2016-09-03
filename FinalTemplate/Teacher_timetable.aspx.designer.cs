c645fa53d0bfbeec876b31e11e2737077bc1ceaa
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-09-03 07:32:00 -0700

    asas

--- FinalTemplate/Teacher_timetable.aspx.designer.cs
+++ FinalTemplate/Teacher_timetable.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace FinalTemplate {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// timetable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource timetable;
     }
 }
