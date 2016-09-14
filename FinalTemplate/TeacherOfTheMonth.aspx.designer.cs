fd796dc69276b7afc417f8605f471660e06586ab
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-09-15 01:08:22 +0500

    all tasks were done IN TIME but couldn't get the internet to commit/sync my work

--- FinalTemplate/TeacherOfTheMonth.aspx.designer.cs
+++ FinalTemplate/TeacherOfTheMonth.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace FinalTemplate {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btn2;
+        
+        /// <summary>
+        /// txt_teacherid control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_teacherid;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
