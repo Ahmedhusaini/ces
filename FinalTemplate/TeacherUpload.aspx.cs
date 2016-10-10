729a349c6cad6ced850e564b662a9c746026342c
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-10-10 02:19:20 -0700

    A

--- FinalTemplate/TeacherUpload.aspx.cs
+++ FinalTemplate/TeacherUpload.aspx.cs
@@ -31,12 +31,14 @@ namespace FinalTemplate
 
                     string a = ConfigurationManager.ConnectionStrings["abc"].ConnectionString;
                     Database db = new Database("abc");
+                    int teacher_id = Convert.ToInt32(db.GetLastValueByColumnName("teacher_id", "tbl_teacher"));
+                    int school_id = Convert.ToInt32(db.GetLastValueByColumnName("School_id", "tbl_school"));
                     using (SqlConnection con = new SqlConnection(a))
                     {
                         con.Open();
-                        SqlCommand cmd = new SqlCommand("INSERT INTO Table_1(name,type,data) VALUES (@name,@type,@data)",con);
+                        SqlCommand cmd = new SqlCommand("INSERT INTO tbl_Teacher_lecture(lectures,type,data) VALUES (@lectures,@type,@data)",con);
                       
-                        cmd.Parameters.AddWithValue("@name", filename);
+                        cmd.Parameters.AddWithValue("@lectures", filename);
                         cmd.Parameters.AddWithValue("@type", filetype);
                         cmd.Parameters.AddWithValue("@size", filesize);
 
