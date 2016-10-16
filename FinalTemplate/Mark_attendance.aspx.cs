22d891d8d6eaac7b27d070a93d99c2f0e460b68f
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-10-16 13:37:02 -0700

    some changes

--- FinalTemplate/Mark_attendance.aspx.cs
+++ FinalTemplate/Mark_attendance.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using FinalTemplate.source.Database;
 
 namespace FinalTemplate
 {
