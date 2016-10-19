8a8927355de23f6ed8a44e5e9eac4d793459a744
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-10-19 12:06:48 -0700

    some changes but still not done

--- FinalTemplate/Mark_attendance.aspx
+++ FinalTemplate/Mark_attendance.aspx
@@ -2,7 +2,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="TeacherHeadPlaceHolder" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="TeacherPlaceHolder1" runat="server">
-                                                                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" DataSourceID="SqlDataSource1" GridLines="Horizontal" Width="293px">
+                                                                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" DataSourceID="SqlDataSource1" GridLines="Horizontal" Width="293px" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
                                     <Columns>
                                         <asp:BoundField DataField="Std_id" HeaderText="Student ID" SortExpression="Std_id" />
                                         <asp:TemplateField HeaderText="Remarks">
@@ -16,6 +16,7 @@
 
                                             </ItemTemplate>
                                         </asp:TemplateField>
+                                        <asp:CommandField ShowSelectButton="True" />
                                     </Columns>
                                     <FooterStyle BackColor="White" ForeColor="#333333" />
                                     <HeaderStyle BackColor="#336666" Font-Bold="True" ForeColor="White" />
@@ -27,11 +28,11 @@
                                     <SortedDescendingCellStyle BackColor="#E5E5E5" />
                                     <SortedDescendingHeaderStyle BackColor="#275353" />
                                 </asp:GridView>
-    <asp:DropDownList ID="DropDownList1" runat="server">
+<%--    <asp:DropDownList ID="DropDownList1" runat="server">
         <asp:ListItem>Present</asp:ListItem>
         <asp:ListItem>Absent</asp:ListItem>
         <asp:ListItem>Leave</asp:ListItem>
-                                                                </asp:DropDownList>
+                                                                </asp:DropDownList>--%>
     
                                                                          
                                     <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:abc %>" SelectCommand="SELECT [Std_id] FROM [tbl_student_attendance]"></asp:SqlDataSource>
--- FinalTemplate/Mark_attendance.aspx.cs
+++ FinalTemplate/Mark_attendance.aspx.cs
@@ -11,18 +11,28 @@ using FinalTemplate.source.Database;
 
 namespace FinalTemplate
 {
-	public partial class mark_attendance : System.Web.UI.Page
-	{
+    public partial class mark_attendance : System.Web.UI.Page
+    {
         protected void Page_Load(object sender, EventArgs e)
         {
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-             string a = ConfigurationManager.ConnectionStrings["abc"].ConnectionString;
+
+
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var sValue3 = GridView1.SelectedRow.Cells[1].Text;
+
+            string a = ConfigurationManager.ConnectionStrings["abc"].ConnectionString;
             Database db = new Database("abc");
             int std_attend_id = Convert.ToInt32(db.GetLastValueByColumnName("std_attend_id", "tbl_student_attendance"));
             var std_id = Convert.ToString(db.GetLastValueByColumnName("std_id", "tbl_Student_Reg"));
+            int remark_id = Convert.ToInt32(db.GetLastValueByColumnName("remark_id", "tbl_remarks"));
 
             using (SqlConnection con = new SqlConnection(a))
             {
@@ -30,22 +40,21 @@ namespace FinalTemplate
                 SqlCommand cmd = new SqlCommand("std_attendance", con);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                cmd.Parameters.AddWithValue("@std_id", SqlDbType.VarChar).Value = std_id;
-                cmd.Parameters.AddWithValue("@std_id_out", SqlDbType.VarChar).Direction = ParameterDirection.Output;
-             
-
-
+                cmd.Parameters.AddWithValue("@remark_id", SqlDbType.VarChar).Value = remark_id;
+                cmd.Parameters.AddWithValue("@remark_id_out", SqlDbType.VarChar).Direction = ParameterDirection.Output;
                 cmd.Parameters.AddWithValue("@std_attend_id", SqlDbType.Int).Value = std_attend_id + 1;
                 cmd.Parameters.AddWithValue("@std_attend_id_out", SqlDbType.Int).Direction = ParameterDirection.Output;
+                cmd.Parameters.AddWithValue("@std_id", SqlDbType.VarChar).Value = std_id;
+                cmd.Parameters.AddWithValue("@std_id_out", SqlDbType.VarChar).Direction = ParameterDirection.Output;
+
                 cmd.Parameters.AddWithValue("@date", SqlDbType.VarChar).Value = DateTime.Now.ToString("");
                 cmd.Parameters.AddWithValue("@time", SqlDbType.VarChar).Value = DateTime.Now.ToString("");
-                cmd.Parameters.AddWithValue("@remarks", SqlDbType.VarChar).Value = DropDownList1.SelectedValue;
+                cmd.Parameters.AddWithValue("@remarks", SqlDbType.VarChar).Value = sValue3;
 
                 cmd.ExecuteNonQuery();
                 con.Close();
-       
-     
+
             }
         }
-	}
+    }
 }
