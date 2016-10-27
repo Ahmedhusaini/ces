44cb389a673bc230fc4e8b5c385cfb26d990c09f
ahmedhusaini <ahmedhusaini94@outlook.com>
2016-10-27 12:38:43 -0700

    profile changed

--- FinalTemplate/Teacherprofile.aspx.cs
+++ FinalTemplate/Teacherprofile.aspx.cs
@@ -7,40 +7,46 @@ using System.IO;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using FinalTemplate.source.Database;
+using FinalTemplate.source.Functions;
 
 namespace FinalTemplate
 {
     public partial class Teacherprofile : System.Web.UI.Page
     {
         SqlConnection con = new SqlConnection("Data Source=AHMED;Initial Catalog=images;Integrated Security=True");
+        private Database myDatabase = new Database("abc");
         protected void Page_Load(object sender, EventArgs e)
         {
+                if (Session["userid"] != null)
+                {
+                    string[] col = { "General_Id" };
+                    string[] colwhere = { "authorized_id" };
+                    string[] whereoperator = { "=" };
+                    string[] multiwhere = { "" };
 
-        }
+                    CurrentUser.GetAuthorizedDetails(Session["userid"].ToString());
+                    string[] whereoperatorvale = { "'" + CurrentUser.AuthorizedID + "'" };
+                    string[,] teacherid = myDatabase.SelectQuery("tbl_teacher", col, colwhere, whereoperator, whereoperatorvale, multiwhere);
+                    CurrentUser.GetPersonalDetails(Convert.ToInt32(teacherid[0, 0]));
+                    name.Text=CurrentUser.Username;
+                    usertype.Text=CurrentUser.UserType;
+                    Gender.Text=CurrentUser.Gender;
+                    Nationality.Text=CurrentUser.Nationality;
+                    Religion.Text=CurrentUser.Religion;
+                    Phone.Text=CurrentUser.Phone;
+                    Address.Text=CurrentUser.Address;
+                    Email.Text=CurrentUser.PrimaryEmailAddress;
+                    Email2.Text = CurrentUser.SecondaryEmailAddress;
+                    
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            if (FileUpload1.HasFile)
-            {
-                string str = FileUpload1.FileName;
-                FileUpload1.PostedFile.SaveAs(Server.MapPath(".") + "//images//" + str);
-                string path = "~//images//" + str.ToString();
-                con.Open();
-                SqlCommand cmd = new SqlCommand("insert into image values('" + TextBox1.Text + "','" + path + "')", con);
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Label1.Text = "Image Uploaded Sucessfully";
-                SqlDataAdapter da = new SqlDataAdapter("select * from image", con);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                GridView1.DataSource = dt;
-                DataBind();
-            }
-            else
-            {
-                Label1.Text = "Upload Your Image";
+                }
 
-            }
+                else
+                {
+                    Response.Redirect("~/Default.aspx");
+                }
+
         }
     }
 }
