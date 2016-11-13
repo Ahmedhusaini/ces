52922cb25a713050ae0d67d067878415b16f6bfa
jahangeerahmed <jahangeer.ahmed11@gmail.com>
2016-11-13 16:19:32 +0500

    teacher of the month photo method added to school web service

--- FinalTemplate/source/WebServices/School.asmx.cs
+++ FinalTemplate/source/WebServices/School.asmx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.Services;
 using FinalTemplate.source.Functions;
+using FinalTemplate.source.Database;
 namespace FinalTemplate.source.WebServices
 {
     /// <summary>
@@ -19,12 +20,13 @@ namespace FinalTemplate.source.WebServices
     public class School : System.Web.Services.WebService
     {
         private SchoolRelatedFunction objSchoolRelatedFunction = new SchoolRelatedFunction();
-
+        private Database.Database myDatabase = new Database.Database("ces");
+        JavaScriptSerializer objSerializer = new JavaScriptSerializer();
+         
         [WebMethod]
         public void GetNumberOfUsers(string _schoolid)
         {
             IDictionary<string, int> usercount = new ConcurrentDictionary<string, int>();
-            JavaScriptSerializer objSerializer = new JavaScriptSerializer();
             usercount["teacher"] = objSchoolRelatedFunction.TotalTeachers(_schoolid);
             usercount["student"] = objSchoolRelatedFunction.TotalStudent(_schoolid);
             usercount["course"] = objSchoolRelatedFunction.TotalCourse(_schoolid);
@@ -32,6 +34,34 @@ namespace FinalTemplate.source.WebServices
                                      objSchoolRelatedFunction.TotalStudent(_schoolid) +
                                      objSchoolRelatedFunction.TotalTeachers(_schoolid);
             HttpContext.Current.Response.Write(objSerializer.Serialize(usercount));
+        }
+
+        [WebMethod]
+        public void Teacherofthemonthphoto(string _schoolid)
+        {
+            IDictionary<string,string> photo=new Dictionary<string, string>();
+            myDatabase.CreateConnection();
+            myDatabase.InitializeSQLCommandObject(myDatabase.GetCurrentConnection, "TeacherOfTheMonthImage",true);
+            myDatabase.obj_sqlcommand.Parameters.AddWithValue("@schoolid", _schoolid);
+            try
+            {
+                myDatabase.OpenConnection();
+                myDatabase.obj_reader = myDatabase.obj_sqlcommand.ExecuteReader();
+                if (myDatabase.obj_reader.HasRows)
+                {
+                    while (myDatabase.obj_reader.Read())
+                    {
+                        photo["photo"] = myDatabase.obj_reader["photo"].ToString();
+                    }
+                }
+            }
+            finally
+            {
+                myDatabase.CloseConnection();
+                myDatabase.obj_reader.Close();
+                myDatabase.obj_reader.Dispose();
+            }
+            HttpContext.Current.Response.Write(objSerializer.Serialize(photo));
         }
 
     }
