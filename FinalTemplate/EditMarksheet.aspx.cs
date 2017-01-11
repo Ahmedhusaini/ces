616113d99b1f4697433ab5ff252b7b0bdff7642e
ahmedhusaini <ahmedhusaini94@outlook.com>
2017-01-12 00:48:23 +0500

    pages added

---
+++ FinalTemplate/EditMarksheet.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalTemplate
+{
+    public partial class EditMarksheet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int m, g, u, i, s, c;
+            m = Convert.ToInt32(TextBox1.Text);
+            g = Convert.ToInt32(TextBox2.Text);
+            u = Convert.ToInt32(TextBox3.Text);
+            i = Convert.ToInt32(TextBox4.Text);
+            s = Convert.ToInt32(TextBox5.Text);
+            c = Convert.ToInt32(TextBox6.Text);
+            TextBox7.Text =Convert.ToString(((m+g+u+i+s+c)/6));
+          
+        }
+    }
+}
