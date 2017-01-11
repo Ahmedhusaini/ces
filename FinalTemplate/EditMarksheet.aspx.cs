ede9caf280fca80f0aa0a515c91fd008859f46ab
ahmedhusaini <ahmedhusaini94@outlook.com>
2017-01-12 00:52:29 +0500

    page was not showing so i commited again

--- FinalTemplate/EditMarksheet.aspx.cs
+++ FinalTemplate/EditMarksheet.aspx.cs
@@ -7,24 +7,23 @@ using System.Web.UI.WebControls;
 
 namespace FinalTemplate
 {
-    public partial class EditMarksheet : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class EditMarksheet : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
+		}
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             int m, g, u, i, s, c;
-            m = Convert.ToInt32(TextBox1.Text);
-            g = Convert.ToInt32(TextBox2.Text);
-            u = Convert.ToInt32(TextBox3.Text);
-            i = Convert.ToInt32(TextBox4.Text);
-            s = Convert.ToInt32(TextBox5.Text);
-            c = Convert.ToInt32(TextBox6.Text);
-            TextBox7.Text =Convert.ToString(((m+g+u+i+s+c)/6));
-          
+            +m = Convert.ToInt32(TextBox1.Text);
+            +g = Convert.ToInt32(TextBox2.Text);
+            +u = Convert.ToInt32(TextBox3.Text);
+            +i = Convert.ToInt32(TextBox4.Text);
+            +s = Convert.ToInt32(TextBox5.Text);
+            +c = Convert.ToInt32(TextBox6.Text);
+            +TextBox7.Text = Convert.ToString(((m + g + u + i + s + c) / 6));
         }
-    }
+	}
 }
