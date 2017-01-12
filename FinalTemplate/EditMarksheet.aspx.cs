4ced3295eb93a0ff7cdbab145df9dcbf6cbe2806
ahmedhusaini <ahmedhusaini94@outlook.com>
2017-01-13 03:30:03 +0500

    aaa

--- FinalTemplate/EditMarksheet.aspx.cs
+++ FinalTemplate/EditMarksheet.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
 
 namespace FinalTemplate
 {
@@ -14,16 +16,29 @@ namespace FinalTemplate
 
 		}
 
+
         protected void Button1_Click(object sender, EventArgs e)
         {
-            int m, g, u, i, s, c;
-            m = Convert.ToInt32(TextBox1.Text);
-            g = Convert.ToInt32(TextBox2.Text);
-            u = Convert.ToInt32(TextBox3.Text);
-            i = Convert.ToInt32(TextBox4.Text);
-            s = Convert.ToInt32(TextBox5.Text);
-            c = Convert.ToInt32(TextBox6.Text);
-            TextBox7.Text = Convert.ToString(((m + g + u + i + s + c) / 6));
-        }
+            if(FileUpload1.HasFile)
+            {
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/files/") + FileUpload1.FileName);
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Files", typeof(string));
+            dt.Columns.Add("Size", typeof(string));
+            dt.Columns.Add("Type", typeof(string));
+
+            //foreach(string strfile in Directory.GetFiles(Server.MapPath("~/files/")));
+            //{
+            //    FileInfo fi = new FileInfo(strfile);
+            //    dt.Rows.Add(fi.Name, fi.Length, fi.Extension);
+
+            //}
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        } 
+
+        //private string getfiletypebyextension(string extension)
 	}
 }
